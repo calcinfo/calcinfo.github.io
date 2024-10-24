e41cce3891b8a73c172d1b097bf8f041ce97a98b
calcinfo <153659699+calcinfo@users.noreply.github.com>
2024-10-24 02:42:57 +0100

    Add files via upload

--- s3.js
+++ s3.js
@@ -1,108 +1,94 @@
+
+function crid(tt,cr,y) {
+if (tt> 10) {
+  return cr;
+} else {
+  return y;
+}
+  
+}
 function calcu() {
-
-
-    var ana3td = Number(document.getElementById('ana3td').value) ;
-
-   
-
-    var ana3ctrl = Number(document.getElementById('ana3ctrl').value);
-
-    var ana3tt = document.getElementById('ana3tt').value = Number((((ana3td)*4)+(ana3ctrl*6))/10);
-
-  var anantd = Number(document.getElementById('anantd').value) ;
+  var probtd = Number(document.getElementById('probtd').value) ;
 
  
 
-  var ananctrl = Number(document.getElementById('ananctrl').value);
+  var probctrl = Number(document.getElementById('probctrl').value);
 
-  var anantp = Number(document.getElementById('anantp').value);
+   var probtt = document.getElementById('probtt').value =  Number((probtd)*0.33+(probctrl*0.67));
+  var algotd = Number(document.getElementById('algotd').value) ;
 
  
 
-  var anantt = document.getElementById('anantt').value =  Number((anantd)*0.2+(ananctrl*0.6)+(anantp)*0.2);
+  var algoctrl = Number(document.getElementById('algoctrl').value);
+
+  var algotp = Number(document.getElementById('algotp').value);
+
+ 
+
+  var algott = document.getElementById('algott').value =  Number((algotd)*0.17+(algoctrl*0.66)+(algotp)*0.17);
 
 
 
 
+    var alg1ntd = Number(document.getElementById('alg1ntd').value) ;
 
-  var ovtd = Number(document.getElementById('ovtd').value) ;
+ 
+
+    var alg1ctrl = Number(document.getElementById('alg1ctrl').value);
+
+    var alg1tt = document.getElementById('alg1tt').value = Number((((alg1ntd)*0.33)+(alg1ctrl*0.67)));
 
 
-  var ovctrl = Number(document.getElementById('ovctrl').value);
+ 
 
-  var ovtp = Number(document.getElementById('ovtp').value);
+
+
+
+    var ana1ntd = Number(document.getElementById('ana1ntd').value) ;
+
+   var ana1ntp = Number(document.getElementById('ana1ntp').value) ;
+
+
+    var ana1ctrl = Number(document.getElementById('ana1ctrl').value);
+
+    var ana1tt = document.getElementById('ana1tt').value = Number((((ana1ntp)*0.2)+((ana1ntd)*0.2)+(ana1ctrl*0.6)));
+  
+var codtd = Number(document.getElementById('codtd').value) ;
 
   
 
-  var ovtt = document.getElementById('ovtt').value = Number((ovtd)*0.2+(ovctrl*0.6)+(ovtp*0.2));
+    var codctrl = Number(document.getElementById('codctrl').value);
 
-
-
-  var mdftd = Number(document.getElementById('mdftd').value) ;
-
-
-  var mdfctrl = Number(document.getElementById('mdfctrl').value);
-
-  var mdftp = Number(document.getElementById('mdftp').value);
+    var codtt = document.getElementById('codtt').value = Number((((codtd)*0.70)+(codctrl*0.30)));
+var coddtd = Number(document.getElementById('coddtd').value) ;
 
   
 
-  var mdftt = document.getElementById('mdftt').value = Number((mdftd)*0.2+(mdfctrl*0.6)+(mdftp*0.2));
+    var coddctrl = Number(document.getElementById('coddctrl').value);
+
+    var coddtt = document.getElementById('coddtt').value = Number((((coddtd)*0.5)+(coddctrl*0.5)));
+
+var fund=document.getElementById("fund").innerHTML = ((2*probtt)+(2*alg1tt)+(3*ana1tt))/7;
+
+  var fundcr= document.getElementById('fundcr').innerHTML = crid(fund,15,0);
+
+var fund2=document.getElementById("fund2").innerHTML = ((3*algott)+(3*codtt)+(2*coddtt))/8;
+
+  var fund2cr= document.getElementById('fund2cr').innerHTML = crid(fund2,13,0);
+
+  var engtt = Number(document.getElementById('engtt').value);
+  var engcr = crid(engtt,2,0);
 
 
-    var mrtd = Number(document.getElementById('mrtd').value) ;
-
-   
-
-    var mrctrl = Number(document.getElementById('mrctrl').value);
-
-    var mrtt = document.getElementById('mrtt').value = Number((((mrtd)*4)+(mrctrl*6))/10);
-
-
-
-  var infott = Number(document.getElementById('infott').value);
-  
 
 
   
 
 
-
-
-
-
-
-
-
-
-
-
-
-  
-  var destt = Number(document.getElementById('destt').value);
-
-
-
-
- var engtd = Number(document.getElementById('engtd').value) ;
-
-   
-
-    var engctrl = Number(document.getElementById('engctrl').value);
-
-    var engtt = document.getElementById('engtt').value = Number((((engtd)*4)+(engctrl*6))/10);
-
-
-
-
-
-
-
-
-
-
-      document.getElementById("tt").innerHTML = ((3*ana3tt)+(3*anantt)+(3*ovtt)+(3*mdftt)+(2*mrtt)+(2*infott)+(destt)+(2*engtt))/19;
+       var toto= document.getElementById('toto').innerHTML = Number((fund*7+fund2*8+engtt)/16);
+  var ttcr= document.getElementById('ttcr').innerHTML = crid(toto,30,(fundcr+fund2cr+engcr));
 
 
 
 }
+
